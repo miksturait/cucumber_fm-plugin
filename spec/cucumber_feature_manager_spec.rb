2bb1e1d81f0d4ba8eb0b34421d1b1bd7ea22e453
cs3b <michalczyz@gmail.com>
2010-04-23 20:30:49 +0200

    typo

--- spec/cucumber_feature_manager_spec.rb
+++ spec/cucumber_feature_manager_spec.rb
@@ -2,5 +2,5 @@ require 'spec_helper'
 
 describe CucumberFeatureManager do
   it "should store path for features"
-  it "should scan files in specyfic directory"
+  it "should scan files in specific directory"
 end
