d22e4a2f199c9cabff3131bb5beae8026a791105
cs3b <michalczyz@gmail.com>
2010-04-26 18:07:01 +0200

    remove unneeded spec

--- spec/cucumber_f_m/feature_all_tags_to_scenario_spec.rb
+++ spec/cucumber_f_m/feature_all_tags_to_scenario_spec.rb
@@ -2,6 +2,5 @@ require 'spec_helper'
 
 describe "Moving all tags from feature to scenarios" do
   it "should move all tags from feature to scenario"
-  it "should remove empty line from feature section"
   it "should preserve single acceptance tags from overwriting"
 end
