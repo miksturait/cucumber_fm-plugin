2e844057128a1623f79981a796ef95c17edce147
cs3b <michalczyz@gmail.com>
2010-04-26 20:48:59 +0200

    tags spec

--- spec/cucumber_f_m/feature_module/tag_spec.rb
+++ spec/cucumber_f_m/feature_module/tag_spec.rb
@@ -1,5 +1,19 @@
 require 'spec_helper'
 
-describe CucumberFM::FeatureModule::Tag do
-  it "should "
+describe "Cucumber::FeatureElement::Component::Tags" do
+  it "should parse tags"
+  it "should fetch module"
+  it "should fetch module from feature if not present"
+  it "should fetch milestone"
+  it "should fetch milestone from feature if not present"
+  it "should fetch status"
+  it "should fetch status from feature if not present"
+  it "should fetch bucket"
+  it "should fetch bucket from feature if not present"
+  it "should fetch estimation"
+  it "should fetch estimation from feature if not present"
+  it "should fetch value"
+  it "should fetch value from feature if not present"
+  it "should fetch developer"
+  it "should fetch developer from feature if not present"
 end
