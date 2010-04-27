7a62e7920891614617faae44458966f636c02075
cs3b <michalczyz@gmail.com>
2010-04-27 21:30:47 +0200

    scenario tittle and comment

--- spec/cucumber_f_m/feature_module/scenario_spec.rb
+++ spec/cucumber_f_m/feature_module/scenario_spec.rb
@@ -1,9 +1,29 @@
 require 'spec_helper'
 
 describe CucumberFM::FeatureElement::Scenario do
-  it "should have access to feature"
-  it "should parse tags"
-  it "should parse comments lines"
-  it "should parse title"
+  before(:all) do
+raw = %Q{## wireframe::http://somelink
+
+  @_todo @hash @wow
+  Scenario: #{@title = "Creating filter scope"}
+    When I follow "New system user"
+    And I fill in "password" for "Password Confirmation"}
+   @feature = CucumberFM::Feature.new('fake_path')
+   @feature.stub(:info).and_return(mock('info'))
+   @feature.info.stub(:tags).and_return([@mc, @_done, @aaa])
+   @scenario = CucumberFM::FeatureElement::Scenario.new(@feature, raw)
+  end
+  it "should have access to feature" do
+    @scenario.feature.should == @feature
+  end
+  it "should parse tags" do
+    @scenario.tags.should == %w(@_todo @hash @wow @mc @aaa)
+  end
+  it "should parse comments lines" do
+    @scenario.should have(1).comments
+  end
+  it "should parse title" do
+    @scenario.title.should == @title
+  end
   it "should parse steps"
 end
