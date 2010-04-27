37416eedbd8e41c2ff3e4236588ac654aec57568
cs3b <michalczyz@gmail.com>
2010-04-27 21:38:53 +0200

    scenario comments expetation

--- lib/cucumber_f_m/feature_element/scenario.rb
+++ lib/cucumber_f_m/feature_element/scenario.rb
@@ -4,6 +4,8 @@ module CucumberFM
       PATTERN = /((^.*#+.*\n)+\n?)?(^.*@+.*\n)?^[ \t]*Scenario:.*\n(^.*\S+.*\n?)*/
 
       include CucumberFM::FeatureElement::Component::Tags
+      include CucumberFM::FeatureElement::Component::Title
+      include CucumberFM::FeatureElement::Component::Comments
 
       def second_tags_source
         feature.info
