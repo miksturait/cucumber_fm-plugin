f32d61d723e54c8e4b86f2a2fbe5415f07ab4377
cs3b <michalczyz@gmail.com>
2010-05-07 14:18:23 +0200

    background

--- lib/cucumber_f_m/feature_element/background.rb
+++ lib/cucumber_f_m/feature_element/background.rb
@@ -2,6 +2,9 @@ module CucumberFM
   module FeatureElement
     class Background < Struct.new(:feature, :raw)
       PATTERN = /((^.*#+.*\n)+\n?)?^[ \t]*Background:.*\n(^.*\S+.*\n?)*/
+
+      include CucumberFM::FeatureElement::Component::Title
+      include CucumberFM::FeatureElement::Component::Comments
     end
   end
 end
