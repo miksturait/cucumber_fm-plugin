00a064c95e530abf1dccdd87a4976948fbe93827
cs3b <michalczyz@gmail.com>
2010-06-04 14:26:19 +0200

    adding missing param

--- app/controllers/documentation/features_controller.rb
+++ app/controllers/documentation/features_controller.rb
@@ -1,5 +1,5 @@
 class Documentation::FeaturesController < Documentation::ApplicationController
   def index
-    @cfm = CucumberFeatureManager.new(feature_dir_path) 
+    @cfm = CucumberFeatureManager.new(feature_dir_path, git_dir_path) 
   end
 end
