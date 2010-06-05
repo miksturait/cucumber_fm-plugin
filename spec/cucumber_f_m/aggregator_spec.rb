4f25f5b1ead75b08aba025fc372eaf512b7e476c
cs3b <michalczyz@gmail.com>
2010-06-05 15:20:55 +0200

    in the middle of aggregator

--- spec/cucumber_f_m/aggregator_spec.rb
+++ spec/cucumber_f_m/aggregator_spec.rb
@@ -1,48 +1,95 @@
 require 'spec_helper'
 
+# TODO understand why subject { Klass.new(a,b) } doesn't work in rspec as it should
+
 describe CucumberFM::Aggregator do
   before(:each) do
     @f1 = mock('feature1')
     @f2 = mock('feature2')
-    @s11 = mock('scenario1', :feature => @f1, :tags => ['@m1', '@mc', '@i1'])
-    @s12 = mock('scenario2', :feature => @f1, :tags => ['@m2', '@ak', '@i1'])
-    @s21 = mock('scenario3', :feature => @f2, :tags => ['@m3', '@mc', '@i1'])
-    @s22 = mock('scenario4', :feature => @f2, :tags => ['@m2', '@tb', '@i2'])
-    @cfm = mock('cfm', :scenarios => [@s11, @s12, @s21, @s22])
+    @s11 = mock('scenario1', :feature => @f1, :tags => ['@m1', '@mc', '@i1', '@1.5'])
+    @s12 = mock('scenario2', :feature => @f1, :tags => ['@m2', '@ak', '@i1', '@1.75'])
+    @s13 = mock('scenario5', :feature => @f1, :tags => ['@m2', '@ak', '@i1', '@1.75'])
+    @s21 = mock('scenario3', :feature => @f2, :tags => ['@m3', '@mc', '@i1', '@2'])
+    @s22 = mock('scenario4', :feature => @f2, :tags => ['@m2', '@tb', '@i2', '@1'])
+    @cfm = mock('cfm', :scenarios => [@s11, @s12, @s13, @s21, @s22])
     @aggregator1 = CucumberFM::FeatureElement::Component::Tags::PATTERN[:milestone]
     @aggregator2 = CucumberFM::FeatureElement::Component::Tags::PATTERN[:iteration]
   end
-  
+
   context "single dimension" do
+    before(:each) do
+      @aggregator = CucumberFM::Aggregator.new(@cfm, @aggregator1)
+      @collection = @aggregator.collection
+    end
     it "should aggregate correctly" do
-      result = CucumberFM::Aggregator.new(@cfm, @aggregator1)
-      result.collection.should == {'@m1' => {
-              @f1 => [@s11]
-      },
-                                   '@m2' => {
-                                           @f1 => [@s12],
-                                           @f2 => [@s22]
-                                   },
-                                   '@m3' => {
-                                           @f2 => [@s21]
-                                   }}
+      @collection.should ==
+              {
+                      '@m1' => {
+                              @f1 => [@s11]
+                      },
+                      '@m2' => {
+                              @f1 => [@s12, @s13],
+                              @f2 => [@s22]
+                      },
+                      '@m3' => {
+                              @f2 => [@s21]
+                      }
+              }
+    end
+
+    {'@m1' =>[1, 1, 1.5], '@m2' => [2, 3, 2.75], '@m3' => [1, 1, 2]}.each_pair do |key, value|
+      context "should correct count" do
+        context key do
+          it "features" do
+            @collection[key].should have(value[0]).features
+          end
+          it "scenarios" do
+            @collection[key].should have(value[1]).scenarios
+          end
+          it "estimation" do
+            @collection[key].estimation.should == value[2]
+          end
+        end
+      end
     end
   end
 
   context "double dimension" do
-    it "should aggregate correctly" do
-      result = CucumberFM::Aggregator.new(@cfm, @aggregator1, @aggregator2)
-      result.collection.should == {'@m1' => {
-              '@i1' => { @f1 => [@s11] }
-      },
-                                   '@m2' => {
-                                           '@i1' => { @f1 => [@s12]},
-                                           '@i2' => { @f2 => [@s22]}
-                                   },
-                                   '@m3' => {
-                                           '@i1' => { @f2 => [@s21]}
-                                   }}
+    before(:each) do
+      @aggregator = CucumberFM::Aggregator.new(@cfm, @aggregator1, @aggregator2)
+      @collection = @aggregator.collection
     end
-  end
+    it "should aggregate correctly" do
+      @collection.should ==
+              {
+                      '@m1' => {
+                              '@i1' => { @f1 => [@s11] }
+                      },
+                      '@m2' => {
+                              '@i1' => { @f1 => [@s12, @s13]},
+                              '@i2' => { @f2 => [@s22]}
+                      },
+                      '@m3' => {
+                              '@i1' => { @f2 => [@s21]}
+                      }
+              }
+    end
 
+    {'@m1' =>[1, 1, 1.5], '@m2' => [2, 3, 2.75], '@m3' => [1, 1, 2]}.each_pair do |key, value|
+      context "should correct count" do
+        context key do
+          it "features" do
+            @collection[key].should have(value[0]).features
+          end
+          it "scenarios" do
+            @collection[key].should have(value[1]).scenarios
+          end
+          it "estimation" do
+            @collection[key].estimation.should == value[2]
+          end
+        end
+      end
+    end
+
+  end
 end
