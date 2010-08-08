d4932828fd4de13be006447cde79d34a64d289b4
Michal Czyz <michalczyz@gmail.com>
2010-08-08 09:06:16 +0200

    refactoring

--- lib/cucumber_f_m/statistic.rb
+++ lib/cucumber_f_m/statistic.rb
@@ -5,31 +5,19 @@ module CucumberFM
       cfm
     end
 
-    def component
-      @module ||= report(:component)
-    end
-
-    def bucket
-      @bucket ||= report(:bucket)
-    end
-
-    def status
-      @status ||= report(:status)
-    end
-
-    def developer
-      @developer ||= report(:developer)
-    end
-
-    def iteration
-      @iteration ||= report(:iteration)
-    end
-
     def various
       @various ||= CucumberFM::Aggregator.new(cfm, nil, CucumberFM::FeatureElement::Component::Tags::TECHNICAL).collection
     end
 
     private
+
+    def method_missing(m, * args, & block)
+      if CucumberFM::FeatureElement::Component::Tags::PATTERN.has_key?(m.to_sym)
+        report(m)
+      else
+        super
+      end
+    end
 
     def report(aggregate_by)
       CucumberFM::Aggregator.new(cfm, (aggregate_by.is_a?(Regexp) ? aggregate_by : patterns(aggregate_by))).collection
