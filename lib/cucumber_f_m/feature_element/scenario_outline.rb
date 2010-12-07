179b7e94fec463c1aba1e5559be92f864cb338a5
Michal Czyz <michalczyz@gmail.com>
2010-12-07 20:39:54 +0100

    * fix - drawing navigation in editor for scenario outline * fix - problem with parsing scenario outline

--- lib/cucumber_f_m/feature_element/scenario_outline.rb
+++ lib/cucumber_f_m/feature_element/scenario_outline.rb
@@ -2,7 +2,8 @@ module CucumberFM
   module FeatureElement
     class ScenarioOutline < Struct.new(:feature, :raw)
 
-      PATTERN = /((^.*#+.*\n)+\n?)?(^.*@+.*\n)?^[ \t]*Scenario Outline:.*\n?(^.*\S+.*\n?)*\n?^[ \t]*Examples:.*\n(^.*\S+.*\n?)*/
+      # TODO requires more specs and some improvements
+      PATTERN = /((^.*#+.*\n)+\n?)?(^.*@+.*\n)?^[ \t]*Scenario Outline:.*\n?((^.*\S+.*\n?)*\n?)?(^[ \t]*Examples:.*\n(^.*\S+.*\n?)*)?/
 
       include CucumberFM::FeatureElement::Component::Tags
       include CucumberFM::FeatureElement::Component::Title
